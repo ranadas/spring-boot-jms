9884861cfa5145c0574a3cfac4979b59e6e3bc85
Rana Das <rana.pratap.das@gmail.com>
2019-05-07 07:30:12 +0100

    Formatting source + add git ignore

--- src/main/java/lankydan/tutorial/repositories/OrderTransactionRepository.java
+++ src/main/java/lankydan/tutorial/repositories/OrderTransactionRepository.java
@@ -3,4 +3,5 @@ package lankydan.tutorial.repositories;
 import lankydan.tutorial.documents.OrderTransaction;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface OrderTransactionRepository extends MongoRepository<OrderTransaction, String> {}
+public interface OrderTransactionRepository extends MongoRepository<OrderTransaction, String> {
+}
--- src/main/java/lankydan/tutorial/rest/OrderTransactionController.java
+++ src/main/java/lankydan/tutorial/rest/OrderTransactionController.java
@@ -3,20 +3,22 @@ package lankydan.tutorial.rest;
 import lankydan.tutorial.documents.OrderTransaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.web.bind.annotation.*;
-
-import java.math.BigDecimal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/transaction")
 public class OrderTransactionController {
 
-  @Autowired private JmsTemplate jmsTemplate;
+    @Autowired
+    private JmsTemplate jmsTemplate;
 
-  @PostMapping("/send")
-  public void send(@RequestBody OrderTransaction transaction) {
-    System.out.println("Sending a transaction.");
-    jmsTemplate.convertAndSend(
-        "OrderTransactionQueue", transaction);
-  }
+    @PostMapping("/send")
+    public void send(@RequestBody OrderTransaction transaction) {
+        System.out.println("Sending a transaction.");
+        jmsTemplate.convertAndSend(
+                "OrderTransactionQueue", transaction);
+    }
 }
